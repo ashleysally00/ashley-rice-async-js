8fbe99657673fbb8086b7b505787553acb20807b
ashleysally00 <74416365+ashleysally00@users.noreply.github.com>
2024-01-07 10:05:19 -0800

    Add files via upload

---
+++ await.js
@@ -0,0 +1,18 @@
+//async function myFunction() {}
+
+async function fetchProducts() {
+  try {
+    const response = await fetch(
+      "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data[0].name);
+  } catch (error) {
+    console.error(`Could not get poducts: ${error}`);
+  }
+}
+
+fetchProducts();
